c6d735b80c66985ecced99e34b1f892b9c0dd2ab
Shamil <shamil.badalloffsh@gmail.com>
2020-05-13 23:48:51 +0200

    Add: arithmetical mean

---
+++ homework 02/arithmeticalMean.js
@@ -0,0 +1,35 @@
+"use strict";
+/**
+ * 4. Среднее арифметическое
+ * Запрашивать у пользователя (prompt) числа пока пользователь не введет пустое значение.
+ * После ввода каждого числа выводить в консоль предварительный итог.
+ * По завершению (когда пользователь ввел пустое значение) отобразить (alert) сумму, количество чисел и среднее арифметическое . 
+ */
+let numbersArray = Array();
+
+function arithmeticalMean() {
+  let inputNumber = prompt('Введите число');
+  let sum;
+  let arMean;
+
+  if (inputNumber) {
+    numbersArray.push(Number(inputNumber));
+    sum = numbersArray.reduce((a, b) => a + b);
+    arMean = sum / 2;
+    displayResult(sum, numbersArray.length, arMean, true);
+  }else{
+    sum = numbersArray.reduce((a, b) => a + b);
+    arMean = sum / 2;
+    displayResult(sum, numbersArray.length, arMean);
+  }
+};
+
+function displayResult(sum, numbersCount, arithmetMean, repeat=false){
+  console.log(`Сумма чисел: ${sum}`);
+  console.log(`Количество чисел: ${numbersCount}`);
+  console.log(`Cреднее арифметическое: ${arithmetMean}`);
+  if (repeat) 
+  {
+    arithmeticalMean();
+  }
+};
