f6af7b885b8f5bc207e823b987b94192bc43cfd1
Shamil <shamil.badalloffsh@gmail.com>
2020-05-19 13:36:02 +0200

    Fix: inserting new lines without cutting the words

--- homework 02 advanced/insertNewLinesInSentence.js
+++ homework 02 advanced/insertNewLinesInSentence.js
@@ -6,7 +6,25 @@
 
 function wrapText() {
   let longText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-  let modifyedText = longText.replace(/(.{80})/g, "$1\n");
-  console.log(longText)
-  console.log(modifyedText);
-}
+
+  let aggregator = [];
+
+  function wrapText(longText) {
+    if (longText.length > 80) {
+      let chunk = longText.substring(0, 80);
+      let index = chunk.lastIndexOf(' ');
+      aggregator.push(longText.substring(0, index));
+      let restOfTheText = longText.substr(index + 1);
+
+      if (restOfTheText != '') {
+        wrapText(restOfTheText);
+      }
+    } else {
+      aggregator.push(longText);
+    }
+  };
+  
+  wrapText(longText);
+
+  console.log(aggregator.join('\n'));
+};
