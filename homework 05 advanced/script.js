ca74e3cd1647be02680d5ee7675dbb8c91738201
Shamil <shamil.badalloffsh@gmail.com>
2020-05-31 22:12:41 +0200

    Add: parsetemplate function + tests

---
+++ homework 05 advanced/script.js
@@ -0,0 +1,57 @@
+'use strict';
+/**
+ * innerHTML и outerHTML не использовать - это слишком просто
+ * 
+ * 1. Создать функцию, которая будет принимать HTML элемент и объект.
+ * Требуется обойти все дочерние ноды HTML элемента и у тех элементов, 
+ * у которых есть атрибут data-field выставить textContent 
+ * из соответствующего свойства объекта.
+ * В случае, если в объекте нет нужного свойства - выбросить ошибку,
+ * если в объекте есть свойство, которое не используется в HTML - игнорировать
+ * Пример:
+ * HTML  
+ * 
+ * <div id=”item1”>
+ *   <h3 data-field=”title”>Some title</h3>
+ *   <p data-field=”description”></p>
+ * </div>
+ * 
+ * JS
+ * 
+ * parseTemplate(
+ *   document.getElementById(‘item1’),
+ *   {
+ *     title: ‘Hello world’,
+ *     description: ‘The first program’,
+ *   }
+ * );
+ * 
+ *  HTML должен измениться на  
+ * 
+ * <div id=”item1”>
+ *   <h3 data-field=”title”>Hello world</h3>
+ *   <p data-field=”description”>The first programm</p>
+ * </div>
+ * 
+ * @param {Node} node
+ * @param deep уровень вложенности
+ * @return {VoidFunction}
+ */
+
+function parseTemplate(el, obj) {
+  if (!el)
+    throw new Error('Please provide element');
+
+  if (!obj)
+    throw new Error('Please provide an object with data');
+
+  let keys = Object.keys(obj);
+
+  for (let i = 0; i < el.children.length; i++) {
+    let currElement = el.children[i];
+    let dataAttributes = currElement.dataset;
+    if (keys.includes(dataAttributes.field)) {
+      currElement.textContent = obj[dataAttributes.field]
+    }
+  }
+}
---
+++ homework 05 advanced/script.test.js
@@ -0,0 +1,33 @@
+describe('parseTemplate', () => {
+  let el = document.getElementById('item1');
+  const data = { title: 'New title', description: 'new description' };
+  const falseData = { falseTitle: 'New fake title', falseDescription: 'New fake description' };
+
+  it('should throw error if no element provided', () => {
+    expect(() => parseTemplate('', {})).to.throw('Please provide element');
+  });
+
+  it('should throw error if no object data provided', () => {
+    expect(() => parseTemplate(el)).to.throw('Please provide an object with data');
+  });
+
+  it('should not throw error if all arguments provided', () => {
+    expect(() => parseTemplate(el, data)).not.to.throw();
+  });
+
+  it('should modifies element correctly', () => {
+    parseTemplate(el, data);
+
+    expect(el.querySelector('h3').textContent).to.equal(data.title);
+    expect(el.querySelector('p').textContent).to.equal(data.description);
+  })
+
+  it('should not modifies element correctly', () => {
+    parseTemplate(el, falseData);
+
+    expect(el.querySelector('h3').textContent).not.to.equal(falseData.title);
+    expect(el.querySelector('p').textContent).not.to.equal(falseData.description);
+    expect(el.querySelector('h3').textContent).to.equal(data.title);
+    expect(el.querySelector('p').textContent).to.equal(data.description);
+  })
+});
