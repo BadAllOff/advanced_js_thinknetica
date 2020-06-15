bcbd2e93effb6ee457108567fb2668dabdfcc9bb
Shamil <shamil.badalloffsh@gmail.com>
2020-06-15 12:22:53 +0200

    Add: copy ship code from homework 07

---
+++ homework 08/ships/core/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @typedef {Object} Ship
+ * @property {string} name
+ * @property {string} model
+ * @property {Object} position
+ * @property {number} distance
+ * @property {number} speed
+ */
+
+/**
+ * @typedef {Object} Marina
+ * @property {Object} position
+ * @property {Array} ships
+ */
---
+++ homework 08/ships/script.js
@@ -0,0 +1,51 @@
+const ship = new Ship("Best ship", "Tesla", { x: 1, y: 1 });
+dropAnchor.call(ship);
+const ship2 = new Ship("Good ship 2");
+const marina = new Marina({ x: 3, y: 3 });
+
+riseAnchor.call(ship);
+ship.moveTo({ x: 3, y: 3 });
+marina.moor(ship);
+marina.unmoor(ship);
+
+// console.log(ship);
+// console.log(ship.getAnchorDroped());
+// // ship._isAnchorDroped = true; Так нельзя
+// console.log(ship);
+// console.log('  after 1 try', ship.getAnchorDroped());
+// ship.speed = 0;
+// ship.setAnchorDroped(true);
+// console.log(ship);
+// console.log(ship.getAnchorDroped());
+
+// const car = {
+//     model: 'Model X',
+//     seats: 4,
+// }
+
+// dropAnchor(ship);
+// dropAnchor(car);
+
+// function dropAnchor(ship) {
+//     console.log(ship);
+//     console.log(typeof ship);
+
+//     if (!(ship instanceof Ship))
+//         throw new Error('Not a Ship');
+
+//     ship.isAnchorDroped = true;
+// }
+
+// Save it
+
+// const arr = {
+//     '0': 'a',
+//     '1': 'b',
+//     length: 2,
+// }
+
+// for (let i = 0; i < arr.length; i++)
+//     console.log(arr[i]);
+
+// Array.from(arr)
+//      .forEach(item => console.log(item))
